ef1f2c39b24b7feecd39a28e93e2ecb27cbca9d5
Dugeri James <dugeri.james@gmail.com>
2022-05-18 21:07:41 +0100

    outdated version of torch.data

--- seq2seq.py
+++ seq2seq.py
@@ -1,17 +1,21 @@
+# most functions deprecated
+
 from tokenize import tokenize
 from turtle import forward
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torchtext.data
 from torchtext.datasets import Multi30k
 from torchtext.data import Field, BucketIterator
 import numpy as np
 import spacy
 import random
-from torch.utils.tensorboard import SummaryWriter
+# from torch.utils.tensorboard import SummaryWriter
+from utils import translate_sentence, bleu, save_checkpoint, load_checkpoint
 
-spacy_ger = spacy.load('de')
-spacy_eng = spacy.load('en')
+spacy_ger = spacy.load('de_core_news_sm')
+spacy_eng = spacy.load('en_core_web_sm')
 
 def tokenizer_ger(text):
     return [tok.text for tok in spacy_ger.tokenizer(text)]
@@ -26,7 +30,7 @@ german = Field(
 english = Field(
     tokenize=tokenizer_eng, lower=True, init_token='<sos>', eos_token='<eos>'
 )
-train_data, validation_data, test_data = Multi30k.splits(exts=('.de', '.en'), fields=(german, english))
+train_data, valid_data, test_data = Multi30k.splits(exts=('.de', '.en'), fields=(german, english))
 
 german.build_vocab(train_data, max_size=10000, min_freq=2)
 english.build_vocab(train_data, max_size=10000, min_freq=2)
@@ -38,15 +42,21 @@ class Encoder(nn.Module):
         self.num_layers = num_layers
         self.dropout = nn.Dropout(p)
         self.embedding =nn.Embedding(input_size, embedding_size)
-        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=p)
+        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, bidrectional=True, dropout=p)
+        self.fc_hidden = nn.Linear(hidden_size*2, hidden_size)
+        self.fc_cell = nn.Linear(hidden_size*2, hidden_size)
 
     def forward(self, x):
         # x.shape: (seq_length, N)
         embedding = self.dropout(self.embedding(x))
         # embedding shape:  (seq_length, N, embedding_size)
 
-        outputs, (hidden, cell) = self.rnn(embedding)
-        return hidden, cell
+        encoder_states, (hidden, cell) = self.rnn(embedding)
+
+        hidden = self.fc_hidden(torch.cat((hidden[0:1], hidden[1:2]), dim=2))
+        cell = self.fc_cell(torch.cat((cell[0:1], cell[1:2]), dim=2))
+
+        return encoder_states, hidden, cell
 
 class Decoder(nn.Module):
     def __init__(self, input_size, embedding_size, hidden_size, output_size, num_layers, p):
@@ -56,27 +66,154 @@ class Decoder(nn.Module):
 
         self.dropout = nn.Dropout(p)
         self.embedding = nn.Embedding(input_size, embedding_size)
-        self.rnn = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=p)
+        self.rnn = nn.LSTM(hidden_size*2 + embedding_size, hidden_size, num_layers, dropout=p)
+        
+        self.energy = nn.Linear(hidden_size*3, 1)
+        self.softmax = nn.Softmax(dim=0)
+        self.relu = nn.ReLU()
         self.fc = nn.Linear(hidden_size, output_size)
         
-    def forward(self, x, hidden, cell):
+    def forward(self, x, encoder_states, hidden, cell):
         # shape x (N), but we want (1, N)
         x = x.unsqueeze(0)
 
         embedding = self.dropout(self.embedding(x))
         # embedding shape: (1, N, embedding_size)
 
-        outputs, (hidden, cell) = self.rnn(embedding, (hidden, cell))
+        seq_len = encoder_states.shape[0]
+        h_reshaped = hidden.repeat(seq_len, 1, 1)
+
+        energy = self.relu(self.energy(torch.cat((h_reshaped, encoder_states), dim=2)))
+        attention = self.softmax(energy)
+
+        context_vector = torch.einsum("snk,snl->knl", attention, encoder_states)
+
+        rnn_input = torch.cat((context_vector, embedding), dim=2)
+        outputs, (hidden, cell) = self.rnn(rnn_input, (hidden, cell))
         # shape of outputs: (1, N, hidden_size)
 
         predictions = self.fc(outputs)
         # shape of predictions: (1, N, length_of_vocab)
-
+ 
         predictions = predictions.squeeze(0)
 
-        return predictions
-
-
+        return predictions, hidden, cell
 
 class Seq2Seq(nn.Module):
-    pass
+    def __init__(self, encoder, decoder):
+        super(Seq2Seq, self).__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, source, target, teacher_force_ratio=0.5):
+        batch_size = source.shape[1]
+        target_len = target.shape[0]
+        target_vocab_size = len(english.vocab)
+
+        outputs = torch.zeros(target_len, batch_size, target_vocab_size).to(device)
+        hidden, cell = self.encoder(source)
+
+        # Grab start token
+        x = target[0]
+
+        for t in range(1, target_len):
+            output, hidden, cell = self.decoder(x, encoder_states, hidden, cell)
+
+            outputs[t] = output
+
+            best_guess = output.argmax(1)
+
+            x = target[t] if random.random() < teacher_force_ratio else best_guess
+
+        return outputs
+
+# Training Hyperparameters
+num_epochs = 100
+learning_rate = 3e-4
+batch_size = 32
+
+#  Model Hyperparameters
+load_model=False
+save_model = True
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+input_size_encoder = len(german.vocab)
+input_size_decoder = len(english.vocab)
+
+output_size = len(english.vocab)
+
+encoder_embedding_size = 300
+decoder_embedding_size = 300
+
+hidden_size = 1024
+num_layers = 1
+enc_dropout = 0.0
+dec_dropout = 0.0
+
+# TensorBoard
+# writer = SummaryWriter(f'runs/loss_plot')
+# step=0
+
+train_iterator, valid_iterator, test_iterator = torchtext.data.BucketIterator.splits(
+    (train_data, valid_data, test_data),
+    batch_size=batch_size,
+    sort_within_batch=True,
+    sort_key = lambda x: len(x.src),
+    device=device)
+
+encoder_net = Encoder(
+    input_size_encoder, encoder_embedding_size,
+    hidden_size, num_layers, enc_dropout).to(device)
+
+decoder_net = Decoder(
+    input_size_decoder, decoder_embedding_size,
+    hidden_size, num_layers, dec_dropout).to(device)
+ 
+model = Seq2Seq(encoder_net, decoder_net).to(device)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+pad_idx = english.voacb.stoi['<pad>']
+criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
+
+if load_model:
+    load_checkpoint(torch.load('my_checkpoint.pth.ptar'), model, optimizer)
+
+sentence = "ein boot mit mehreren männern darauf wird von einem großen pferdegespann ans ufer gezogen."
+
+
+for epoch in range(num_epochs):
+    print(f'Epoch [{epoch} / {num_epochs}]')
+
+    checkpoint = {'state_dict': model.state_dict(), 'optimizer': optimizer.state_dict()}
+    save_checkpoint(checkpoint)
+
+    model.eval()
+    translated_sentence = translate_sentence(model, sentence, german, english, device, max_length=50)
+
+    print(f'Tranlated Sentence: \n {translated_sentence}')
+
+    model.train()
+
+    for batch_idx, batch in enumerate(train_iterator):
+        inp_data = batch.src.to(device)
+        target = batch.trg.to(device)
+
+        output = model(inp_data, target)
+        # output shape: (trg_len, batch_size, output_dim)
+
+        output = output[1:].reshape(-1, output.shape[2])
+        target = target[1:].reshape(-1)
+
+        optimizer.zero_grad()
+        loss = criterion(output, target)
+
+        loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+        optimizer.step()
+
+        # writer.add_scalar('Training loss', loss, global_step =step)
+        # step += 1
+
+score = bleu(test_data[1:100], model, german, english, device)
+print(f'Bleu score {score*100:.2f}')
