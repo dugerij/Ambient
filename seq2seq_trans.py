5abad06a8c2cdac93ba4d191e1bd5ae7751ded89
Dugeri James <dugeri.james@gmail.com>
2022-05-19 10:01:20 +0100

    new

--- seq2seq_trans.py
+++ seq2seq_trans.py
@@ -16,4 +16,20 @@ token_transform[SRC_LANGUAGE] = get_tokenizer('spacy', language='de_core_news_sm
 token_transform[TGT_LANGUAGE] = get_tokenizer('spacy', language='en_core_web_sm')
 
 # helper function to yield list of tokens
-def
+def yield_tokens(data_iter: Iterable, language:str) -> List[str]:
+    language_index = {SRC_LANGUAGE: 0, TGT_LANGUAGE: 1}
+
+    for data_sample in data_iter:
+        yield token_transform[language](data_sample[language_index[language]])
+
+UNK_IDX, PAD_IX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
+special_symbols = ['<unk>', '<pad>', '<bos>', '<eos>']
+
+for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
+    # Training data iterator
+    train_iter = Multi30k(split='train', language_pair=(SRC_LANGUAGE, TGT_LANGUAGE))
+
+    vocab_transform[ln] = build_vocab_from_iterator(yield_tokens(train_iter, ln),min_freq=1, specials=special_symbols, special_first=True)
+
+for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
+    vocab_transform[ln].set_default_index(UNK_IDX)
