9f4238a8625635a6ea71b29dee5b25b1e890f048
Dugeri James <dugeri.james@gmail.com>
2022-05-19 09:51:51 +0100

    NEw

--- seq2seq_trans.py
+++ seq2seq_trans.py
@@ -1,2 +1,19 @@
 # implemented from https://pytorch.org/tutorials/beginner/translation_transformer.html
 
+from socketserver import ThreadingTCPServer
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from torchtext.datasets import Multi30k
+from typing import Iterable, List
+
+SRC_LANGUAGE = 'de'
+TGT_LANGUAGE = 'en'
+
+token_transform = {}
+vocab_transform = {}
+
+token_transform[SRC_LANGUAGE] = get_tokenizer('spacy', language='de_core_news_sm')
+token_transform[TGT_LANGUAGE] = get_tokenizer('spacy', language='en_core_web_sm')
+
+# helper function to yield list of tokens
+def
